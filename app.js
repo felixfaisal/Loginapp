3dd17664cd51819ee2b1dbe35909ad984cc74844
felixfaisal <42486737+felixfaisal@users.noreply.github.com>
2019-10-08 03:51:37 +0530

    Registering in a local variable

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcryptjs');
 app.set('view engine','ejs')
 app.use('/assets',express.static(__dirname+'/assets'));
 app.use(express.urlencoded({extended:false}));
+var users = [];
 
 app.get('/',(req,res)=>{
   res.render('home');
@@ -13,8 +15,20 @@ app.get('/login',(req,res)=>{
 app.get('/register',(req,res)=>{
   res.render('register');
 })
-app.post('/register',(req,res)=>{
-  console.log(req.body);
+app.post('/register',async (req,res)=>{
+  try{
+    const hash = await bcrypt.hash(req.body.password,10);
+    users.push({
+      name: req.body.username,
+      email: req.body.email,
+      password: hash
+    });
+    console.log(users);
+    res.redirect('login');
+  }
+  catch{
+    res.redirect('register');
+  }
 })
 app.listen(3000);
 console.log('Listening at 3000');
