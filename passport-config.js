3c9a40c3ba28d88413eaabfcfa1d42089ff11a3e
felixfaisal <42486737+felixfaisal@users.noreply.github.com>
2019-10-10 00:32:51 +0530

    Authenticating using passport

--- passport-config.js
+++ passport-config.js
@@ -1,6 +1,26 @@
 const lpass = require('passport-local');
-
+const bcrypt = require('bcryptjs').strategy;
 function initialize(passport){
+  const authenticateuser =(username,password)=>{
+    const user = getUserByUsername(username);
+    if(user==null){
+      return done(null,false,{message:"No such user or password"});
 
+    }
+    try{
+      if(bcrypt.compare(password,user.password)){
+
+      }else{
+        return done(null,false,{message:'Password Incorrect'});
+      }
+      }
+      catch(e){
+        return done(e);
+      }
+    }
+
+  passport.use(new lpass(),authenticateuser);
+  passport.serializeUser((user,done)=>{});
+  passport.deserializeUser((id,done)=>{});
 }
 module.exports = initialize;
